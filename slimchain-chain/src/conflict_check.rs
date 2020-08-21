7204f2eeef03cabdb5be835bf1b1cb1c4960df46
Cheng XU <git@xuc.me>
2020-08-20 20:14:46 -0700

    slimchain-chain: update conflict_check

--- slimchain-chain/src/conflict_check.rs
+++ slimchain-chain/src/conflict_check.rs
@@ -1,92 +1,125 @@
 use crate::access_map::{AccessMap, RevAccessItem};
+use serde::Deserialize;
 use slimchain_common::{
     basic::BlockHeight,
     rw_set::{TxReadSet, TxWriteData},
 };
 
-pub trait ConflictCheck {
-    fn has_conflict(
-        access_map: &AccessMap,
-        tx_block_height: BlockHeight,
-        reads: &TxReadSet,
-        writes: &TxWriteData,
-    ) -> bool;
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ConflictCheck {
+    OCC,
+    SSI,
 }
 
-pub struct OCCConflictCheck;
-
-impl ConflictCheck for OCCConflictCheck {
-    fn has_conflict(
+impl ConflictCheck {
+    pub fn has_conflict(
+        self,
         access_map: &AccessMap,
         tx_block_height: BlockHeight,
         reads: &TxReadSet,
         writes: &TxWriteData,
     ) -> bool {
-        for (&acc_addr, acc_read) in reads.iter() {
-            let write_rev_map = match access_map.get_write_rev(acc_addr) {
-                Some(entry) => entry,
-                None => continue,
-            };
-
-            if write_rev_map.has_conflict_in_read_set(tx_block_height, acc_read) {
-                return true;
-            }
+        match self {
+            Self::OCC => occ_conflict_check(access_map, tx_block_height, reads, writes),
+            Self::SSI => ssi_conflict_check(access_map, tx_block_height, reads, writes),
         }
-
-        for (&acc_addr, acc_write) in writes.iter() {
-            let write_rev_map = match access_map.get_write_rev(acc_addr) {
-                Some(entry) => entry,
-                None => continue,
-            };
-
-            if write_rev_map.has_conflict_in_write_set(tx_block_height, acc_write) {
-                return true;
-            }
-        }
-
-        false
     }
 }
 
-pub struct SSIConflictCheck;
+fn occ_conflict_check(
+    access_map: &AccessMap,
+    tx_block_height: BlockHeight,
+    reads: &TxReadSet,
+    writes: &TxWriteData,
+) -> bool {
+    for (&acc_addr, acc_read) in reads.iter() {
+        let write_rev_map = match access_map.get_write_rev(acc_addr) {
+            Some(entry) => entry,
+            None => continue,
+        };
 
-impl ConflictCheck for SSIConflictCheck {
-    fn has_conflict(
-        access_map: &AccessMap,
-        tx_block_height: BlockHeight,
-        reads: &TxReadSet,
-        writes: &TxWriteData,
-    ) -> bool {
-        let mut flag1 = false;
-        let mut flag2 = false;
+        if write_rev_map.has_conflict_in_read_set(tx_block_height, acc_read) {
+            return true;
+        }
+    }
 
-        for (&acc_addr, acc_write) in writes.iter() {
-            let write_rev_map = match access_map.get_write_rev(acc_addr) {
-                Some(entry) => entry,
-                None => continue,
-            };
+    for (&acc_addr, acc_write) in writes.iter() {
+        let write_rev_map = match access_map.get_write_rev(acc_addr) {
+            Some(entry) => entry,
+            None => continue,
+        };
 
-            if write_rev_map.has_conflict_in_write_set(tx_block_height, acc_write) {
-                return true;
-            }
+        if write_rev_map.has_conflict_in_write_set(tx_block_height, acc_write) {
+            return true;
+        }
+    }
 
-            let read_rev_map = match access_map.get_read_rev(acc_addr) {
-                Some(entry) => entry,
-                None => continue,
-            };
+    false
+}
 
-            flag1 |= read_rev_map.has_conflict_in_write_set(tx_block_height, acc_write);
+fn ssi_conflict_check(
+    access_map: &AccessMap,
+    tx_block_height: BlockHeight,
+    reads: &TxReadSet,
+    writes: &TxWriteData,
+) -> bool {
+    let mut flag1 = false;
+    let mut flag2 = false;
+
+    for (&acc_addr, acc_write) in writes.iter() {
+        let write_rev_map = match access_map.get_write_rev(acc_addr) {
+            Some(entry) => entry,
+            None => continue,
+        };
+
+        if write_rev_map.has_conflict_in_write_set(tx_block_height, acc_write) {
+            return true;
         }
 
-        for (&acc_addr, acc_read) in reads.iter() {
-            let write_rev_map = match access_map.get_write_rev(acc_addr) {
-                Some(entry) => entry,
-                None => continue,
-            };
+        let read_rev_map = match access_map.get_read_rev(acc_addr) {
+            Some(entry) => entry,
+            None => continue,
+        };
 
-            flag2 |= write_rev_map.has_conflict_in_read_set(tx_block_height, acc_read);
+        flag1 |= read_rev_map.has_conflict_in_write_set(tx_block_height, acc_write);
+    }
+
+    for (&acc_addr, acc_read) in reads.iter() {
+        let write_rev_map = match access_map.get_write_rev(acc_addr) {
+            Some(entry) => entry,
+            None => continue,
+        };
+
+        flag2 |= write_rev_map.has_conflict_in_read_set(tx_block_height, acc_read);
+    }
+
+    flag1 && flag2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_deserialize() {
+        use slimchain_utils::toml;
+
+        #[derive(Deserialize)]
+        struct Test {
+            conflict_check: ConflictCheck,
         }
 
-        flag1 && flag2
+        let input = toml::toml! { conflict_check = "occ" };
+        assert_eq!(
+            ConflictCheck::OCC,
+            input.try_into::<Test>().unwrap().conflict_check
+        );
+
+        let input = toml::toml! { conflict_check = "ssi" };
+        assert_eq!(
+            ConflictCheck::SSI,
+            input.try_into::<Test>().unwrap().conflict_check
+        );
     }
 }
