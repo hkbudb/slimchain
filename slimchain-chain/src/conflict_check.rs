fe9a8cb1054fd4c2afa4b32003032eec557832d4
Cheng XU <git@xuc.me>
2020-08-21 18:48:25 -0700

    slimchain-chain:add test for conflict check

--- slimchain-chain/src/conflict_check.rs
+++ slimchain-chain/src/conflict_check.rs
@@ -100,6 +100,7 @@ fn ssi_conflict_check(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use slimchain_common::{create_tx_read_set, create_tx_write_set};
 
     #[test]
     fn test_deserialize() {
@@ -121,5 +122,94 @@ mod tests {
             ConflictCheck::SSI,
             input.try_into::<Test>().unwrap().conflict_check
         );
+    }
+
+    #[test]
+    fn test_conflict_check() {
+        let mut map = AccessMap::new(2);
+        map.alloc_new_block();
+        map.add_read(&create_tx_read_set! {
+            "0000000000000000000000000000000000000000" => {
+                values: [
+                    "0000000000000000000000000000000000000000000000000000000000000010",
+                ]
+            },
+        });
+        map.add_write(&create_tx_write_set! {
+            "0000000000000000000000000000000000000000" => {
+                values: {
+                    "0000000000000000000000000000000000000000000000000000000000000001" => 1,
+                }
+            },
+        });
+        let _ = map.remove_oldest_block();
+        map.alloc_new_block();
+        map.add_read(&create_tx_read_set! {
+            "0000000000000000000000000000000000000000" => {
+                values: [
+                    "0000000000000000000000000000000000000000000000000000000000000010",
+                ]
+            },
+        });
+        map.add_write(&create_tx_write_set! {
+            "0000000000000000000000000000000000000000" => {
+                values: {
+                    "0000000000000000000000000000000000000000000000000000000000000000" => 1,
+                }
+            },
+        });
+        let _ = map.remove_oldest_block();
+        map.alloc_new_block();
+
+        let tx1_read = create_tx_read_set! {
+            "0000000000000000000000000000000000000000" => {
+                values: [
+                    "0000000000000000000000000000000000000000000000000000000000000010",
+                ]
+            },
+        };
+        let tx1_write = create_tx_write_set! {
+            "0000000000000000000000000000000000000000" => {
+                values: {
+                    "0000000000000000000000000000000000000000000000000000000000000010" => 1,
+                }
+            },
+        };
+        assert!(!ConflictCheck::OCC.has_conflict(&map, 1.into(), &tx1_read, &tx1_write));
+        assert!(!ConflictCheck::SSI.has_conflict(&map, 1.into(), &tx1_read, &tx1_write));
+
+        let tx2_read = create_tx_read_set! {
+            "0000000000000000000000000000000000000000" => {
+                values: [
+                    "0000000000000000000000000000000000000000000000000000000000000000",
+                ]
+            },
+        };
+        let tx2_write = create_tx_write_set! {
+            "0000000000000000000000000000000000000000" => {
+                values: {
+                    "0000000000000000000000000000000000000000000000000000000000000011" => 1,
+                }
+            },
+        };
+        assert!(ConflictCheck::OCC.has_conflict(&map, 1.into(), &tx2_read, &tx2_write));
+        assert!(!ConflictCheck::SSI.has_conflict(&map, 1.into(), &tx2_read, &tx2_write));
+
+        let tx3_read = create_tx_read_set! {
+            "0000000000000000000000000000000000000000" => {
+                values: [
+                    "0000000000000000000000000000000000000000000000000000000000000000",
+                ]
+            },
+        };
+        let tx3_write = create_tx_write_set! {
+            "0000000000000000000000000000000000000000" => {
+                values: {
+                    "0000000000000000000000000000000000000000000000000000000000000010" => 1,
+                }
+            },
+        };
+        assert!(ConflictCheck::OCC.has_conflict(&map, 1.into(), &tx3_read, &tx3_write));
+        assert!(ConflictCheck::SSI.has_conflict(&map, 1.into(), &tx3_read, &tx3_write));
     }
 }
