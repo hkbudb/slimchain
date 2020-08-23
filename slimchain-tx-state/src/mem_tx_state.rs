286d24c55ed191a6b3f9b8ba91435a290dbb3bbd
Cheng XU <git@xuc.me>
2020-08-22 19:40:02 -0700

    impl TxStateView for Arc<dyn TxStateView>

--- slimchain-tx-state/src/mem_tx_state.rs
+++ slimchain-tx-state/src/mem_tx_state.rs
@@ -89,17 +89,3 @@ impl TxStateView for MemTxState {
             .context("Unknown node")
     }
 }
-
-impl TxStateView for Arc<MemTxState> {
-    fn account_trie_node(&self, node_address: H256) -> Result<TrieNode<AccountData>> {
-        self.as_ref().account_trie_node(node_address)
-    }
-
-    fn state_trie_node(
-        &self,
-        acc_address: Address,
-        node_address: H256,
-    ) -> Result<TrieNode<StateValue>> {
-        self.as_ref().state_trie_node(acc_address, node_address)
-    }
-}
--- slimchain-tx-state/src/view/tx_state_view.rs
+++ slimchain-tx-state/src/view/tx_state_view.rs
@@ -1,3 +1,4 @@
+use alloc::sync::Arc;
 use slimchain_common::{
     basic::{AccountData, Address, StateValue, H256},
     error::Result,
@@ -12,4 +13,18 @@ pub trait TxStateView {
         acc_address: Address,
         node_address: H256,
     ) -> Result<TrieNode<StateValue>>;
+}
+
+impl<T: TxStateView> TxStateView for Arc<T> {
+    fn account_trie_node(&self, node_address: H256) -> Result<TrieNode<AccountData>> {
+        self.as_ref().account_trie_node(node_address)
+    }
+
+    fn state_trie_node(
+        &self,
+        acc_address: Address,
+        node_address: H256,
+    ) -> Result<TrieNode<StateValue>> {
+        self.as_ref().state_trie_node(acc_address, node_address)
+    }
 }
