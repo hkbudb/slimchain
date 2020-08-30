7a920d2600121e31468a738c77212950de5f246c
Cheng XU <git@xuc.me>
2020-08-29 22:02:04 -0700

    slimchain-utils: add metrics

--- slimchain-utils/src/lib.rs
+++ slimchain-utils/src/lib.rs
@@ -1,5 +1,8 @@
 pub mod config;
 pub mod contract;
+pub mod metrics;
 pub mod path;
 
+pub use chrono;
 pub use toml;
+pub use tracing;
---
+++ slimchain-utils/src/metrics.rs
@@ -0,0 +1,106 @@
+use slimchain_common::error::{ensure, Result};
+use std::{
+    fs,
+    io::{BufWriter, Write},
+    path::Path,
+    sync::atomic::{AtomicBool, Ordering},
+};
+use tracing::{Dispatch, Level};
+use tracing_appender::non_blocking::WorkerGuard as TracingLoggerGuard;
+
+static METRICS_DISPATCH_INIT: AtomicBool = AtomicBool::new(false);
+pub static mut METRICS_DISPATCH: Option<(Dispatch, TracingLoggerGuard)> = None;
+
+#[macro_export]
+macro_rules! use_metrics_subscriber {
+    ($x:tt) => {
+        if let Some((dispatch, _)) = unsafe { $crate::metrics::METRICS_DISPATCH.as_ref() } {
+            $crate::tracing::dispatcher::with_default(dispatch, || $x)
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! record_time {
+    (label: $label:literal, $time:expr) => {
+        match $time {
+            t => {
+                $crate::use_metrics_subscriber! {{
+                    $crate::tracing::trace!(
+                        kind = "time",
+                        label = $label,
+                        time_ms = (t.as_millis() as u64),
+                    );
+                }};
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! record_event {
+    (label: $label:literal, id: $id:expr) => {
+        match $id {
+            id => {
+                $crate::use_metrics_subscriber! {{
+                    $crate::tracing::trace!(
+                        kind = "event",
+                        label = $label,
+                        id = id,
+                        timestamp = $crate::chrono::Utc::now()
+                            .to_rfc3339_opts($crate::chrono::SecondsFormat::Millis, true)
+                            .as_str(),
+                    );
+                }};
+            }
+        }
+    };
+}
+
+pub fn init_metrics_subscriber(writer: impl Write + Send + Sync + 'static) -> Result<()> {
+    ensure!(
+        !METRICS_DISPATCH_INIT.compare_and_swap(false, true, Ordering::SeqCst),
+        "Metrics subscriber already init."
+    );
+
+    let (writer, guard) = tracing_appender::non_blocking(writer);
+    let subscriber = tracing_subscriber::fmt()
+        .json()
+        .with_max_level(Level::TRACE)
+        .with_writer(writer)
+        .without_time()
+        .finish();
+
+    unsafe {
+        METRICS_DISPATCH = Some((Dispatch::new(subscriber), guard));
+    }
+
+    Ok(())
+}
+
+pub fn init_metrics_subscriber_using_file(file: impl AsRef<Path>) -> Result<()> {
+    let f = fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(file)?;
+    let w = BufWriter::new(f);
+    init_metrics_subscriber(w)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::time::Duration;
+
+    #[test]
+    fn test() {
+        let _ = tracing_subscriber::fmt::try_init();
+        let _ = init_metrics_subscriber(std::io::stdout());
+
+        let time = Duration::from_millis(2200);
+        record_time!(label: "test_time", time);
+        record_event!(label: "test_event", id: 1);
+        tracing::error!("An error");
+        tracing::info!("An info");
+    }
+}
