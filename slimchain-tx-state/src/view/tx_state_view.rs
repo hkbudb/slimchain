fdbec4219423c7d37aff2d216c50f7de054f2187
Cheng XU <git@xuc.me>
2020-09-14 19:38:34 -0700

    slimchain-tx-state: export TrieNode

--- slimchain-tx-state/src/view/tx_state_view.rs
+++ slimchain-tx-state/src/view/tx_state_view.rs
@@ -3,7 +3,7 @@ use slimchain_common::{
     basic::{AccountData, Address, StateValue, H256},
     error::Result,
 };
-use slimchain_merkle_trie::storage::TrieNode;
+pub use slimchain_merkle_trie::storage::TrieNode;
 
 pub trait TxStateView {
     fn account_trie_node(&self, node_address: H256) -> Result<TrieNode<AccountData>>;
