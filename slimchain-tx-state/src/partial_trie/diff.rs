1b0362364b0b4969b2de1b89ebcdbb8c47456e5a
Cheng XU <git@xuc.me>
2020-12-03 20:20:17 -0800

    update draw

--- slimchain-tx-state/src/partial_trie/diff.rs
+++ slimchain-tx-state/src/partial_trie/diff.rs
@@ -24,6 +24,47 @@ pub struct TxTrieDiff {
     pub(crate) acc_trie_diffs: HashMap<Address, AccountTrieDiff>,
 }
 
+impl TxTrieDiff {
+    #[cfg(feature = "draw")]
+    pub fn to_graph(
+        &self,
+        name: impl alloc::string::ToString,
+    ) -> slimchain_merkle_trie::draw::MultiGraph {
+        use alloc::format;
+        use slimchain_merkle_trie::draw::*;
+
+        let name = name.to_string();
+        let mut graph = MultiGraph::new(name.clone());
+        let mut main_trie_diff_graph = MultiGraph::from_partial_trie_diff(
+            format!("{}_main_trie_diff", name),
+            &self.main_trie_diff,
+        );
+        main_trie_diff_graph.set_label("main_trie_diff");
+        graph.add_sub_multi_graph(&main_trie_diff_graph);
+
+        for (i, (acc_addr, acc_trie_diff)) in self.acc_trie_diffs.iter().enumerate() {
+            let mut acc_trie_diff_graph = MultiGraph::from_partial_trie_diff(
+                format!("{}_acc_trie_diff_{}", name, i),
+                &acc_trie_diff.state_trie_diff,
+            );
+            acc_trie_diff_graph.set_label(format!(
+                "addr = {}\nnonce = {:?}\ncode_hash = {:?}",
+                acc_addr, acc_trie_diff.nonce, acc_trie_diff.code_hash,
+            ));
+            graph.add_sub_multi_graph(&acc_trie_diff_graph);
+        }
+
+        graph
+    }
+
+    #[cfg(feature = "draw")]
+    pub fn draw(&self, path: impl AsRef<std::path::Path>) -> Result<()> {
+        use slimchain_merkle_trie::draw::*;
+        let graph = self.to_graph("tx_trie_diff");
+        draw_dot(graph.to_dot(false), path)
+    }
+}
+
 fn merge_acc_trie_diff(lhs: &AccountTrieDiff, rhs: &AccountTrieDiff) -> AccountTrieDiff {
     debug_assert_eq!(lhs.nonce, rhs.nonce);
     debug_assert_eq!(lhs.code_hash, rhs.code_hash);
