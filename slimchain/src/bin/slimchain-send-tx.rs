b67bf7d5bb184cc98522929a6fb8a857750174b3
Cheng XU <git@xuc.me>
2020-12-09 18:20:08 -0800

    send-tx: reduce array size for ioheavy

--- slimchain/src/bin/slimchain-send-tx.rs
+++ slimchain/src/bin/slimchain-send-tx.rs
@@ -97,7 +97,7 @@ impl ContractArg {
             ContractArg::IoHeavy => {
                 let op_gen = Uniform::new(1, 4);
                 let key_gen = Uniform::new(1, 100_000);
-                let size_gen = Uniform::new(1, 32);
+                let size_gen = Uniform::new(1, 8);
                 let contract = self.get_contract();
                 match op_gen.sample(rng) {
                     1 => contract.encode_tx_input(
