5e01448ac293a4726db49012565b7bb58dda8824
Cheng XU <git@xuc.me>
2021-01-07 21:55:29 -0800

    improve build on non-linux

--- slimchain/src/bin/slimchain-node-tee.rs
+++ slimchain/src/bin/slimchain-node-tee.rs
@@ -1,18 +1,28 @@
 use slimchain_common::error::Result;
 use slimchain_tx_engine::TxEngine;
-use slimchain_utils::{config::Config, tx_engine_threads};
+use slimchain_utils::config::Config;
 use std::path::PathBuf;
 
 use slimchain_tee_sig::TEESignedTx as Tx;
-use slimchain_tx_engine_tee::{TEEConfig, TEETxEngineWorkerFactory};
 
+#[cfg(target_os = "linux")]
 fn create_tx_engine(cfg: &Config, enclave: &Option<PathBuf>) -> Result<TxEngine<Tx>> {
+    use slimchain_tx_engine_tee::{TEEConfig, TEETxEngineWorkerFactory};
+    use slimchain_utils::tx_engine_threads;
+
     let tee_cfg: TEEConfig = cfg.get("tee")?;
     let factory = match enclave {
         Some(enclave) => TEETxEngineWorkerFactory::new(tee_cfg, enclave)?,
         None => TEETxEngineWorkerFactory::use_enclave_in_the_same_dir(tee_cfg)?,
     };
     Ok(TxEngine::new(tx_engine_threads(), || factory.worker()))
+}
+
+#[cfg(not(target_os = "linux"))]
+fn create_tx_engine(_cfg: &Config, _enclave: &Option<PathBuf>) -> Result<TxEngine<Tx>> {
+    use slimchain_common::error::bail;
+
+    bail!("not support!");
 }
 
 #[tokio::main]
