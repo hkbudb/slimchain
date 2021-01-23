15cc6a11356d101ec9173c6241246afe44f51d7d
Cheng XU <git@xuc.me>
2021-01-23 01:32:05 -0800

    refactor

--- slimchain-utils/src/serde.rs
+++ slimchain-utils/src/serde.rs
@@ -1,16 +1,15 @@
 use serde::{Deserialize, Serialize};
 use slimchain_common::error::{Error, Result};
 use snap::{read::FrameDecoder, write::FrameEncoder};
-use std::io::Cursor;
 
 pub fn binary_encode<T: Serialize>(value: &T) -> Result<Vec<u8>> {
-    let mut encoder = FrameEncoder::new(Cursor::new(Vec::new()));
+    let mut encoder = FrameEncoder::new(Vec::new());
     bincode::serialize_into(&mut encoder, value).map_err(Error::msg)?;
-    Ok(encoder.into_inner()?.into_inner())
+    Ok(encoder.into_inner()?)
 }
 
 pub fn binary_decode<T: for<'de> Deserialize<'de>>(bytes: &[u8]) -> Result<T> {
-    let decoder = FrameDecoder::new(Cursor::new(bytes));
+    let decoder = FrameDecoder::new(bytes);
     bincode::deserialize_from(decoder).map_err(Error::msg)
 }
 
