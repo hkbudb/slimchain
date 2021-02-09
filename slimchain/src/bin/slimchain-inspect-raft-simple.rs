364a13b4a9e08b10390b99cc5e781972c9d6531d
Cheng XU <git@xuc.me>
2021-02-08 21:48:21 -0800

    add slimchain-inspect-raft-*

---
+++ slimchain/src/bin/slimchain-inspect-raft-simple.rs
@@ -0,0 +1,9 @@
+use slimchain_common::error::Result;
+
+use slimchain_chain::consensus::raft::Block;
+use slimchain_common::tx::SignedTx as Tx;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    slimchain::inspect::inspect_main::<Tx, Block>().await
+}
