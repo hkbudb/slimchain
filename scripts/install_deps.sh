bd251e48d8458cd4c0bc8d3c0c5e6a9194d46b4b
Cheng XU <git@xuc.me>
2021-02-13 00:55:19 -0800

    update scripts

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -13,8 +13,13 @@ if [[ "$EUID" -ne 0 ]]; then
     error "Please run as root."
 fi
 
-if [[ "$(cat /etc/issue)" != *"Ubuntu 18.04"* ]]; then
-    error "Ubuntu 18.04 is required."
+RELEASE_INFO="$(cat /etc/issue)"
+if [[ "$RELEASE_INFO" = *"Ubuntu 18.04"* ]]; then
+    OS="Ubuntu-18.04"
+elif [[ "$RELEASE_INFO" = *"Ubuntu 20.04"* ]]; then
+    OS="Ubuntu-20.04"
+else
+    error "Ubuntu 18.04 or Ubuntu 20.04 is required."
 fi
 
 apt-get update -y
@@ -23,7 +28,16 @@ apt-get install -y build-essential autoconf libtool libssl-dev pkg-config
 apt-get install -y llvm-dev libclang-dev clang # required by bindgen
 apt-get install -y curl gnupg
 
-echo "deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu bionic main" > /etc/apt/sources.list.d/intel-sgx.list
+case "OS" in
+    "Ubuntu-18.04")
+        echo "deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu bionic main" > /etc/apt/sources.list.d/intel-sgx.list
+        SGX_SDK_URL="https://download.01.org/intel-sgx/latest/linux-latest/distro/ubuntu18.04-server/sgx_linux_x64_sdk_$SGX_SDK_VERSION.bin"
+        ;;
+    "Ubuntu-20.04")
+        echo "deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu focal main" > /etc/apt/sources.list.d/intel-sgx.list
+        SGX_SDK_URL="https://download.01.org/intel-sgx/latest/linux-latest/distro/ubuntu20.04-server/sgx_linux_x64_sdk_$SGX_SDK_VERSION.bin"
+        ;;
+esac
 curl -fsSL https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key | apt-key add -
 curl -sL https://deb.nodesource.com/setup_15.x | bash -
 
@@ -31,8 +45,7 @@ apt-get update -y
 apt-get install -y libsgx-uae-service libsgx-urts sgx-aesm-service
 apt-get install -y nodejs
 
-curl -fsSL "https://download.01.org/intel-sgx/latest/linux-latest/distro/ubuntu18.04-server/sgx_linux_x64_sdk_$SGX_SDK_VERSION.bin" \
-    -o /tmp/sgx_linux_x64_sdk.bin
+curl -fsSL "$SGX_SDK_URL" -o /tmp/sgx_linux_x64_sdk.bin
 chmod +x /tmp/sgx_linux_x64_sdk.bin
 mkdir -p /opt/intel
 cd /opt/intel
--- scripts/install_sgx_driver.sh
+++ scripts/install_sgx_driver.sh
@@ -13,12 +13,18 @@ if [[ "$EUID" -ne 0 ]]; then
     error "Please run as root."
 fi
 
-if [[ "$(cat /etc/issue)" != *"Ubuntu 18.04"* ]]; then
-    error "Ubuntu 18.04 is required."
+RELEASE_INFO="$(cat /etc/issue)"
+if [[ "$RELEASE_INFO" = *"Ubuntu 18.04"* ]]; then
+    OS="Ubuntu-18.04"
+    SGX_DERIVER_URL="https://download.01.org/intel-sgx/latest/linux-latest/distro/ubuntu18.04-server/sgx_linux_x64_driver_$SGX_DRIVER_VERSION.bin"
+elif [[ "$RELEASE_INFO" = *"Ubuntu 20.04"* ]]; then
+    OS="Ubuntu-20.04"
+    SGX_DERIVER_URL="https://download.01.org/intel-sgx/latest/linux-latest/distro/ubuntu20.04-server/sgx_linux_x64_driver_$SGX_DRIVER_VERSION.bin"
+else
+    error "Ubuntu 18.04 or Ubuntu 20.04 is required."
 fi
 
-curl -fsSL "https://download.01.org/intel-sgx/latest/linux-latest/distro/ubuntu18.04-server/sgx_linux_x64_driver_$SGX_DRIVER_VERSION.bin" \
-    -o /tmp/sgx_linux_x64_driver.bin
+curl -fsSL "$SGX_DERIVER_URL" -o /tmp/sgx_linux_x64_driver.bin
 chmod +x /tmp/sgx_linux_x64_driver.bin
 mkdir -p /opt/intel
 /tmp/sgx_linux_x64_driver.bin
